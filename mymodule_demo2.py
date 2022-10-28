ae313305a66c58d1e4bb547a0d7905e02f621490
Kilas <89505319494@mail.ru>
2022-10-29 04:22:59 +0500

    modified module_demo2

--- mymodule_demo2.py
+++ mymodule_demo2.py
@@ -1,7 +1,7 @@
-# from mymodule import sayhi, __version__  # так же можно from mymodule import
+from mymodule import sayhi, __version__  # так же можно from mymodule import
 
-# sayhi()
-# print('Версия', __version__)
+sayhi()
+print('Версия', __version__)
 
 
 import this
